6d387e399ae7252c926a5f64b5df0f1d97de42eb
Nmukiti <nmukiti@kabarak.com>
2022-03-10 02:12:40 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,9 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	sizeof("and that piece of art is useful\" -Dora Korpar, 2015-10-19"));
+	char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, "%s\n", n);
+
 	return (1);
 }
