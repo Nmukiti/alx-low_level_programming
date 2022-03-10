aeb8a2dfb4adb638045beadb7818e9644c031686
Nmukiti <nmukiti@kabarak.com>
2022-03-10 00:45:50 -0800

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 int main(void)
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
